45d85ad5b5e22e92cb7c1fb7c4c154d7d16d542c
BaoWoW <baonguyen9392@gmail.com>
2023-06-27 03:40:17 -0700

    Add Send Message Functionality
    User can send messages via formpage on route '/new'.
    After user inputs info and submits, router.post() runs and reroutes back to index page.

--- express-Message-Board/routes/index.js
+++ express-Message-Board/routes/index.js
@@ -13,5 +13,11 @@ const messages = [{
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Message Board', messages });
 });
-
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'Form', messages });
+});
+router.post('/new',function(req, res, next) {
+  messages.push({text: req.body.text, user: req.body.user, added: new Date()})
+  res.redirect('/')
+})
 module.exports = router;
