0e074b02cd7c3e8a41ce273dcb6e8130c3c755b9
BaoWoW <baonguyen9392@gmail.com>
2023-06-28 04:16:11 -0700

    Connected App To MongoDB & Deployed On Fly.io

--- express-Message-Board/routes/index.js
+++ express-Message-Board/routes/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 var express = require('express');
 var router = express.Router();
+let mongoose = require('mongoose');
 const messages = [{
   text: 'Hi there!',
   user:'Amando',
@@ -9,6 +11,16 @@ const messages = [{
   user:'Charles',
   added: new Date(),
 }]
+
+mongoose.connect(process.env.mongodb_URI,{
+  useNewUrlParse: true,
+  useUnifiedTopology: true
+}).then(()=> {
+  console.log('Connected to MongoDB')
+}).catch(err => {
+  console.log(err)
+})
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Message Board', messages });
